a0109e571d1ad650d213052caf5166713936d969
RehanAhmed-dotcom <rehanahmed0406@gmail.com>
2022-08-03 00:02:27 +0500

    about in work and nav done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import "./header.css";
+import CTA from './CTA';
+import ME from '../../assets/Rehan.jpg'
+import HeaderSocials from './HeaderSocials';
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+    
+    <div className="container header__container">
+    
+    <h5>Hello I'm</h5>
+    <h1>Rehan Ahmed</h1>
+    <div className="text-light">
+      Fullstack Developer
+    </div>
+    <CTA />
+    <HeaderSocials />
+    <div className="me">
+    <img src={ME} alt="img"/>  
+    </div>
+    <a href="#contact" className="scroll__down">
+      Scroll Down
+    </a>
+    </div>
+    
+    
+   </header>
   )
 }
 
