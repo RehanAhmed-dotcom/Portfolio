af3e65ea8d0c8fff9f19cd7a8c9569d4880ba009
RehanAhmed-dotcom <rehanahmed0406@gmail.com>
2022-08-18 00:21:33 +0500

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "./portfolio.css";
+
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+// import IMG1 from '../../assets/portfolio1.jpg'
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -9,7 +17,82 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <h3> This is portfolio item title</h3>
+            <img src={IMG1} alt="" />
+          </div>
+          <h3> This is portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://githib.com" className="btn" target={"_blank"}>
+              Github
+            </a>
+            <a
+              href="https://githib.com"
+              className="btn btn-primary"
+              target={"_blank"}
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3> This is portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://githib.com" className="btn" target={"_blank"}>
+              Github
+            </a>
+            <a
+              href="https://githib.com"
+              className="btn btn-primary"
+              target={"_blank"}
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3> This is portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://githib.com" className="btn" target={"_blank"}>
+              Github
+            </a>
+            <a
+              href="https://githib.com"
+              className="btn btn-primary"
+              target={"_blank"}
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3> This is portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://githib.com" className="btn" target={"_blank"}>
+              Github
+            </a>
+            <a
+              href="https://githib.com"
+              className="btn btn-primary"
+              target={"_blank"}
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3> This is portfolio item title</h3>
+          <div className="portfolio__item-cta">
             <a href="https://githib.com" className="btn" target={"_blank"}>
               Github
             </a>
